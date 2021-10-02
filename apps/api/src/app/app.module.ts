31c92648f23bb33df801e3ebeecba8836eb924a6
Bram Borggreve <borggreve@gmail.com>
2021-10-02 00:13:59 -0500

    feat: host static content (placeholder) on api root

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,26 @@
 import { ApiCoreFeatureModule } from '@kin-data/api/core/feature'
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { ensureDirSync, existsSync, writeFileSync } from 'fs-extra'
+import { join } from 'path'
+
+const rootPath = join(__dirname, '..', 'web')
 
 @Module({
-  imports: [ApiCoreFeatureModule],
+  imports: [
+    ApiCoreFeatureModule,
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api/*'],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    if (!existsSync(rootPath)) {
+      ensureDirSync(rootPath)
+      writeFileSync(join(rootPath, 'index.html'), `<pre>kin-data api</pre>`)
+      Logger.verbose(`Created static root path ${rootPath}`)
+    }
+  }
+}
