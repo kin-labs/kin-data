6c61d3939b413ed72a180da2e019ba46b6deaada
beeman <borggreve@gmail.com>
2022-08-08 20:07:07 +0200

    feat: add Prisma (#11)
    * feat: add prisma
    
    * feat: pull schema from database

--- libs/api/core/data-access/src/lib/api-core-data-access.service.ts
+++ libs/api/core/data-access/src/lib/api-core-data-access.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class ApiCoreDataAccessService {
-  constructor() {}
+export class ApiCoreDataAccessService extends PrismaClient implements OnModuleDestroy, OnModuleInit {
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
 
   uptime(): number {
     return process.uptime()
